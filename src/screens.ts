c5968cf5087b145919c16da97df089818d35b2e6
Nhat Nguyen <natt.lefevre@gmail.com>
2024-09-29 02:10:50 -0700

    fix order

--- src/screens.ts
+++ src/screens.ts
@@ -1,26 +1,31 @@
+/* eslint-disable perfectionist/sort-objects */
+
 // ========================================================================== /
 // Natt Nguyen's Tailwind screen v2
 // Last updated: 2024.09.14
 // ========================================================================== /
 
+// Order matters
+// Needs to be greatest to least
+
 // Override to use max width and root font size of 16px (1rem = 16px)
 export const screens = {
-  "320": { max: "320px" },
-  "480": { max: "480px" },
-  "640": { max: "640px" },
-  "768": { max: "768px" },
-  "1024": { max: "1024px" },
-  "1280": { max: "1280px" },
   "1440": { max: "1440px" },
+  "1280": { max: "1280px" },
+  "1024": { max: "1024px" },
+  "768": { max: "768px" },
+  "640": { max: "640px" },
+  "480": { max: "480px" },
+  "320": { max: "320px" },
 }
 
 // Override to use max width and root font size of 10px (1rem = 10px)
 export const screens10 = {
-  "320": { max: "320px" },
-  "480": { max: "480px" },
-  "640": { max: "640px" },
-  "768": { max: "768px" },
-  "1024": { max: "1024px" },
-  "1280": { max: "1280px" },
   "1440": { max: "1440px" },
+  "1280": { max: "1280px" },
+  "1024": { max: "1024px" },
+  "768": { max: "768px" },
+  "640": { max: "640px" },
+  "480": { max: "480px" },
+  "320": { max: "320px" },
 }
